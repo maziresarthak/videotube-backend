7a167d983fe4a09549f6c5e171481c060a384b4a
maziresarthak <sarthakmazire@outlook.com>
2026-08-04 12:43:54 +0530

    add the basic setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from me!");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+// Configuring the dotenv file
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 7001;
+app.listen(7001, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
