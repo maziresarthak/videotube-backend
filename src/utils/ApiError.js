070385e9e60d37f6a6d7172c3e9e20f03220cacf
maziresarthak <sarthakmazire@outlook.com>
2026-08-11 19:37:43 +0530

    add standardise apiError

---
+++ src/utils/ApiError.js
@@ -0,0 +1,16 @@
+// Standardise apiError
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+  }
+}
