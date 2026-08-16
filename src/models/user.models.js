95f1853f7e7380a92699d705a60084ae78adb319
maziresarthak <sarthakmazire@outlook.com>
2026-08-16 19:10:40 +0530

    add the pre hook for password attached to userSchema

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -46,5 +47,13 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
