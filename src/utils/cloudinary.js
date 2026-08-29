97fbd0df3efddab4fa7525b81ad63057448b0e27
maziresarthak <sarthakmazire@outlook.com>
2026-08-30 00:31:07 +0530

    add dotenv package and handle errors

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // configure cloudinary
 cloudinary.config({
@@ -20,6 +23,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
+    console.log("Error on Cloudinary ", error);
     fs.unlinkSync(localFilePath);
     return null;
   }
