8ae95758f69ee6da5b79c648b74916aff19895a4
maziresarthak <sarthakmazire@outlook.com>
2026-09-15 17:45:43 +0530

    add createTweet method

---
+++ src/controllers/tweet.controllers.js
@@ -0,0 +1,27 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const createTweet = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+
+  if (!content || content.trim() === "") {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const { owner } = req.user;
+
+  const tweet = await Tweet.create({
+    content,
+    owner: owner._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, tweet, "Tweet created successfully"));
+});
+
+export { createTweet };
