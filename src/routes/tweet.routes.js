0f6aff6a19f431b7fe71ef039e0f777d6e273dca
maziresarthak <sarthakmazire@outlook.com>
2026-09-15 17:49:58 +0530

    setup the create tweets route

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createTweet } from "../controllers/tweet.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+router.use(verifyJWT); // Apply the verifyJWT middleware to all routes in this file
+
+router.route("/").post(createTweet);
+
+export default router;
