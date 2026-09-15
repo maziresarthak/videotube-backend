3e6ce66b888b5c6a0c8b4d165e4f1cbf26f4e05c
maziresarthak <sarthakmazire@outlook.com>
2026-09-15 17:50:21 +0530

    import the tweer routes

--- src/app.js
+++ src/app.js
@@ -20,9 +20,11 @@ app.use(cookieParser());
 // import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import tweetRoutes from "./routes/tweet.routes.js";
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/tweets", tweetRoutes);
 
 export { app };
