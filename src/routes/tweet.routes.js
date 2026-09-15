d96d05ef534369b9cf6323911f72036b2f28414a
maziresarthak <sarthakmazire@outlook.com>
2026-09-15 23:00:57 +0530

    setup getUserTweets route

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createTweet } from "../controllers/tweet.controllers.js";
+import {
+  createTweet,
+  getUserTweets,
+} from "../controllers/tweet.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply the verifyJWT middleware to all routes in this file
 
 router.route("/").post(createTweet);
+router.route("/t/:userId").get(getUserTweets);
 
 export default router;
