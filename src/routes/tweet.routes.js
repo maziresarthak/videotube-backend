2c1bbc1b2cff1f503f49179e03ce272ad882a219
maziresarthak <sarthakmazire@outlook.com>
2026-09-16 19:48:52 +0530

    setup updateTweet route

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
   createTweet,
   getUserTweets,
+  updateTweet,
+  deleteTweet,
 } from "../controllers/tweet.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -10,5 +12,7 @@ router.use(verifyJWT); // Apply the verifyJWT middleware to all routes in this f
 
 router.route("/").post(createTweet);
 router.route("/t/:userId").get(getUserTweets);
+router.route("/update-tweet/:tweetId").patch(updateTweet);
+router.route("/delete-tweet/:tweetId").delete(deleteTweet);
 
 export default router;
