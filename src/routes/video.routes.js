64ebe7c37beef1bcfa596c88b8bad633674c7e9d
maziresarthak <sarthakmazire@outlook.com>
2026-09-20 18:14:38 +0530

    setup / route for video

---
+++ src/routes/video.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { publishAVideo } from "../controllers/video.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/").post(
+  upload.fields([
+    {
+      name: "videoFile",
+      maxCount: 1,
+    },
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+  ]),
+  publishAVideo
+);
+
+export default router;
