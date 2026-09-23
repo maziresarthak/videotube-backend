df2b8bdf20307d1accf91e413d67e99c36441095
maziresarthak <sarthakmazire@outlook.com>
2026-09-23 12:44:41 +0530

    add deleteVideo method

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -223,7 +223,7 @@ const updateVideo = asyncHandler(async (req, res) => {
   const video = await Video.findOneAndUpdate(
     {
       _id: videoId,
-      owner: req.user._id,
+      owner: req.user?._id,
     },
     {
       $set: {
@@ -250,7 +250,29 @@ const updateVideo = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, video, "Video updated successfully"));
 });
 
-const deleteVideo = asyncHandler(async (req, res) => {});
+const deleteVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const video = await Video.findOneAndDelete({
+    _id: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!video) {
+    throw new ApiError(
+      404,
+      "Video not found or you are not the owner of this video"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Video deleted successfully"));
+});
 
 const togglePublishStatus = asyncHandler(async (req, res) => {});
 
