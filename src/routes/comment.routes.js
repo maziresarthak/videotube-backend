cc516b98ce8b358751055185e22cd0f0fef5c075
maziresarthak <sarthakmazire@outlook.com>
2026-09-24 14:27:29 +0530

    setup updateComment method route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
   addComment,
   getVideoComments,
+  updateComment,
+  deleteComment,
 } from "../controllers/comment.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -10,5 +12,6 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/:videoId").post(addComment).get(getVideoComments);
+router.route("/c/:commentId").patch(updateComment).delete(deleteComment);
 
 export default router;
