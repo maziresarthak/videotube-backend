536807bc68d32d0cae968758a291483678ddca7d
maziresarthak <sarthakmazire@outlook.com>
2026-09-24 14:14:13 +0530

    setup getVideoComments method route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { addComment } from "../controllers/comment.controllers.js";
+import {
+  addComment,
+  getVideoComments,
+} from "../controllers/comment.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/:videoId").post(addComment);
+router.route("/:videoId").post(addComment).get(getVideoComments);
 
 export default router;
