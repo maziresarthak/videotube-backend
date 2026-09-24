6f5c1f33a021478154ea2731a65d90fbd30a01bb
maziresarthak <sarthakmazire@outlook.com>
2026-09-24 12:58:52 +0530

    setup comments routes

--- src/app.js
+++ src/app.js
@@ -22,11 +22,13 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import tweetRoutes from "./routes/tweet.routes.js";
 import videoRoutes from "./routes/video.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/tweets", tweetRoutes);
 app.use("/api/v1/videos", videoRoutes);
+app.use("/api/v1/comments", commentRoutes);
 
 export { app };
