7eb39f0adf53e8976520a6a599cab554ee42f116
maziresarthak <sarthakmazire@outlook.com>
2026-09-25 21:38:21 +0530

    setup toggleSubscription method route

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -2,13 +2,18 @@ import { Router } from "express";
 import {
   getUserChannelSubscribers,
   getSubscribedChannels,
+  toggleSubscription,
 } from "../controllers/subscription.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/u/:channelId").get(getUserChannelSubscribers);
+router
+  .route("/u/:channelId")
+  .get(getUserChannelSubscribers)
+  .post(toggleSubscription);
+
 router.route("/c/:subscriberId").get(getSubscribedChannels);
 
 export default router;
