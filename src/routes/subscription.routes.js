b8d25631da9f82aef7d80bb9c649fc340a24d76a
maziresarthak <sarthakmazire@outlook.com>
2026-09-25 20:52:48 +0530

    setup getSubscribedChannels method route

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getUserChannelSubscribers } from "../controllers/subscription.controllers.js";
+import {
+  getUserChannelSubscribers,
+  getSubscribedChannels,
+} from "../controllers/subscription.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/c/:channelId").get(getUserChannelSubscribers);
+router.route("/u/:channelId").get(getUserChannelSubscribers);
+router.route("/c/:subscriberId").get(getSubscribedChannels);
 
 export default router;
