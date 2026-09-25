124a03d45ef6ae4d91ed827d0a23de1469cb529c
maziresarthak <sarthakmazire@outlook.com>
2026-09-25 13:09:26 +0530

    add getPlaylistById method

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -117,9 +117,28 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, playlists, "Playlists retrieved successfully"));
 });
 
+const getPlaylistById = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlistId");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist retrieved successfully"));
+});
+
 export {
   createPlaylist,
   addVideoToPlaylist,
   removeVideoFromPlaylist,
   getUserPlaylists,
+  getPlaylistById,
 };
