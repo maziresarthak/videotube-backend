d1b8b533a674ad12f96cfa2f0aa714298f186d8d
maziresarthak <sarthakmazire@outlook.com>
2026-09-25 13:03:22 +0530

    setup getUserPlaylists method route

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -3,6 +3,7 @@ import {
   createPlaylist,
   addVideoToPlaylist,
   removeVideoFromPlaylist,
+  getUserPlaylists,
 } from "../controllers/playlist.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -14,5 +15,7 @@ router.route("/").post(createPlaylist);
 
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+
+router.route("/user/:userId").get(getUserPlaylists);
 
 export default router;
