9b173fca9f9fc7a3a239125733764cf02927ef93
maziresarthak <sarthakmazire@outlook.com>
2026-09-25 12:48:29 +0530

    setup removeVideoFromPlaylist method route

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createPlaylist,
   addVideoToPlaylist,
+  removeVideoFromPlaylist,
 } from "../controllers/playlist.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -12,5 +13,6 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 router.route("/").post(createPlaylist);
 
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
+router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
 
 export default router;
