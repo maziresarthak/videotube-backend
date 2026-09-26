e10b7f43c5f31f68a0af4859b83cbdea1f079439
maziresarthak <sarthakmazire@outlook.com>
2026-09-26 12:46:26 +0530

    setup getLikedVideos method route

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -3,6 +3,7 @@ import {
   toggleVideoLike,
   toggleCommentLike,
   toggleTweetLike,
+  getLikedVideos,
 } from "../controllers/like.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -12,5 +13,6 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
 router.route("/toggle/c/:commentId").post(toggleCommentLike);
 router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/videos").get(getLikedVideos);
 
 export default router;
