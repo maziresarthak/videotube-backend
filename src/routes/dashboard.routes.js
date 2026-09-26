4cd428e163b6a74f554cfa23a9f5b3f0ae3ba65d
maziresarthak <sarthakmazire@outlook.com>
2026-09-26 16:57:36 +0530

    setup getChannelVideos route

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { getChannelStats } from "../controllers/dashboard.controllers.js";
+import {
+  getChannelStats,
+  getChannelVideos,
+} from "../controllers/dashboard.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
@@ -7,5 +10,6 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos);
 
 export default router;
