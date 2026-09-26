19d2f1a2115f73ad16223a706973f34ce3798b52
maziresarthak <sarthakmazire@outlook.com>
2026-09-26 12:07:39 +0530

    setup like controller routes

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import videoRoutes from "./routes/video.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
 import playlistRoutes from "./routes/playlist.routes.js";
 import subscriptionRoutes from "./routes/subscription.routes.js";
+import likeRoutes from "./routes/like.routes.js";
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter);
@@ -34,5 +35,6 @@ app.use("/api/v1/videos", videoRoutes);
 app.use("/api/v1/comments", commentRoutes);
 app.use("/api/v1/playlist", playlistRoutes);
 app.use("/api/v1/subscription", subscriptionRoutes);
+app.use("/api/v1/likes", likeRoutes);
 
 export { app };
