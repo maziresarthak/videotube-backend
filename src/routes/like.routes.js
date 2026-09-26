64355fa1cef1f21f87656eb6704f3ea69a85be7f
maziresarthak <sarthakmazire@outlook.com>
2026-09-26 12:32:23 +0530

    setup toggleTweetLike method

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   toggleVideoLike,
   toggleCommentLike,
+  toggleTweetLike,
 } from "../controllers/like.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -10,5 +11,6 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
 router.route("/toggle/c/:commentId").post(toggleCommentLike);
+router.route("/toggle/t/:tweetId").post(toggleTweetLike);
 
 export default router;
