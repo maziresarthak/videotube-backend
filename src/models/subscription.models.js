a0b5209aa243b93b45308d66a24694d539ef3b6f
maziresarthak <sarthakmazire@outlook.com>
2026-09-26 12:10:13 +0530

    update subscription model

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,14 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({
-  subscriber: {
-    type: Schema.Types.ObjectId, // one who IS SUBSCRIBING
-    ref: "User",
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who IS SUBSCRIBING
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one to who a 'subscriber' IS SUBSCRIBING
+      ref: "User",
+    },
   },
-  channel: {
-    type: Schema.Types.ObjectId, // one to who a 'subscriber' IS SUBSCRIBING
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
